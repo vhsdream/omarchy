e10b947f47102e386afdbb34906791b4009b8814
vhsdream <punk.sand7393@fastmail.com>
2025-08-19 12:15:48 -0400

    tweaks-5: kitty

--- install/desktop/theme.sh
+++ install/desktop/theme.sh
@@ -25,8 +25,8 @@ for f in ~/.local/share/omarchy/themes/*; do ln -nfs "$f" ~/.config/omarchy/them
 
 # Set initial theme
 mkdir -p ~/.config/omarchy/current
-ln -snf ~/.config/omarchy/themes/tokyo-night ~/.config/omarchy/current/theme
-ln -snf ~/.config/omarchy/current/theme/backgrounds/1-scenery-pink-lakeside-sunset-lake-landscape-scenic-panorama-7680x3215-144.png ~/.config/omarchy/current/background
+ln -snf ~/.config/omarchy/themes/nord ~/.config/omarchy/current/theme
+ln -snf ~/.config/omarchy/current/theme/backgrounds/1-nord.png ~/.config/omarchy/current/background
 
 # Set specific app links for current theme
 ln -snf ~/.config/omarchy/current/theme/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
